b8cbdf378a5fb6d4dc5c9e2372a4c9b4e9a7e5e7
Abe D <iaryiou111@gmail.com>
2019-03-05 16:00:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,9 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {railroads: {
-	  pieces: 4
+	  pieces: 4,
+	  names: {},
+	  rent_in_dollars: {},
 	}
 	}
 
@@ -22,7 +24,13 @@ end
 def monopoly_with_third_tier
 	monopoly = {railroads: {
 	  pieces: 4,
-	  rent_in_dollars: {}
+	  names: {},
+	  rent_in_dollars: {
+	    one_piece_owned: 25,
+	    two_pieces_owned: 50,
+	    three_pieces_owned: 100,
+	    four_pieces_owned: 200
+	  }
 	}
 	}
 end
